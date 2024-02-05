693a17753bdfc8da62c931410eeba79c5d6a2873
sdac <sdac@sdac-PC>
2024-02-05 19:58:21 +0530

    new update

--- sdac/src/sdac/Main.java
+++ sdac/src/sdac/Main.java
@@ -3,5 +3,10 @@ package sdac;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Home page");
+		System.out.println("Home page1");
+		System.out.println("Home page2");
+		System.out.println("Home page3");
+		System.out.println("Home page4");
+		System.out.println("Home page5");
 	}
 }
