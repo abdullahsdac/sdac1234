b3ec36b0ccf00bdeba112fd47e54705fa2a98217
sdac <sdac@sdac-PC>
2024-02-05 20:00:37 +0530

    sdafdsa

--- sdac/src/sdac/Main.java
+++ sdac/src/sdac/Main.java
@@ -5,8 +5,6 @@ public class Main {
 		System.out.println("Home page");
 		System.out.println("Home page1");
 		System.out.println("Home page2");
-		System.out.println("Home page3");
-		System.out.println("Home page4");
-		System.out.println("Home page5");
+
 	}
 }
